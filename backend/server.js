13ed180dd45df876ab8ad19a7a3832ec568c507f
Sobhan Niroula <sobhan_niroula@yahoo.com>
2022-08-23 01:21:15 +0300

    add api endpoints and env file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((x) => x._id === req.params.id);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
