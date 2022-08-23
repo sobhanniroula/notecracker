38e6fff0e408d389091bec84d35eae98085e21a3
Sobhan Niroula <sobhan_niroula@yahoo.com>
2022-08-23 19:18:05 +0300

    add mainscreen for all the notes pages

--- frontend/src/components/MyNotes/MyNotes.jsx
+++ frontend/src/components/MyNotes/MyNotes.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import MainScreen from "../MainScreen/MainScreen";
 
 const MyNotes = () => {
-  return <h1>My Notes</h1>;
+  return (
+    <MainScreen title="Welcome back">
+      <Link to="/createnote">
+        <Button
+          style={{
+            marginLeft: 10,
+            marginBottom: 6,
+            padding: 10,
+            textTransform: "uppercase",
+            fontSize: 16,
+          }}
+          size="lg"
+          variant="success"
+        >
+          Create New Note
+        </Button>
+      </Link>
+    </MainScreen>
+  );
 };
 
 export default MyNotes;
