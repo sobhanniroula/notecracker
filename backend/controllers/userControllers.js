6a4b92be1edd9a054508671061ba56d030387f9f
Sobhan Niroula <sobhan_niroula@yahoo.com>
2022-08-24 19:19:31 +0300

    add models, routes, controllers and middlewares in the backend

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,35 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, picture } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    picture,
+  });
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      picture: user.picture,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occured!!");
+  }
+});
+
+module.exports = { registerUser };
